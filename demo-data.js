76616e7387717b004e6ee20eab26eb5ff48d6c43
Nguyen Hoang Phuc Huy <hoangphuchuy.nguyen@zuhlke.com>
2026-04-25 01:25:45 +0700

    Refactor

---
+++ demo-data.js
@@ -0,0 +1,47 @@
+// Demo data – replace with real values before deploying
+window.siteData = {
+  boyName: "Anh iu",
+  girlName: "Bé iu",
+  boyImage: "images/boy.png",
+  girlImage: "images/girl.png",
+  // ISO date string for the anniversary (YYYY-MM-DD)
+  startDate: "2024-02-14",
+  // Gallery images (will cycle through this list)
+  image: [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+    "images/9.jpg",
+    "images/10.jpg",
+    "images/11.jpg",
+    "images/12.jpg",
+    "images/13.jpg",
+    "images/14.jpg",
+    "images/15.jpg",
+    "images/16.jpg",
+    "images/17.jpg",
+    "images/18.jpg",
+    "images/19.jpg",
+    "images/20.jpg",
+    "images/21.jpg",
+    "images/22.jpg",
+    "images/23.jpg",
+    "images/24.jpg",
+    "images/25.jpg",
+    "images/26.jpg",
+    "images/27.jpg",
+    "images/28.jpg",
+    "images/29.jpg"
+  ],
+  // Love letter popup title
+  titleMessage: "Happy Valentine's Day 💘",
+  // Love letter content (supports line breaks with \n)
+  message: "Cảm ơn em đã đến bên anh, làm cuộc sống của anh trở nên đặc biệt hơn bao giờ hết.\n\nMỗi ngày bên em là một ngày tuyệt vời, mỗi khoảnh khắc cùng em đều là kỷ niệm đáng trân trọng.\n\nValentine này và mãi mãi về sau, anh chỉ muốn nói với em rằng:\nAnh yêu em nhiều lắm! Thương em nhiều. Mãi mãi yêu em ❤️",
+  // Background music URL (MP3)
+  music: "https://cdn.shopify.com/s/files/1/0757/9700/4572/files/0210.MP3?v=1770658101"
+};
